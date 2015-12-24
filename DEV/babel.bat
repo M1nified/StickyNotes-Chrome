fbbbdc823aa46a47a517638311c868220cb91462
M1nified <M1nified@f6b76eba-76ec-4392-87ed-a92b3dc2a7f6>
2015-12-24 12:38:58 +0000

    minor project watcher changes
    git-svn-id: https://subversion.assembla.com/svn/prowebject.stickynotes/CHROME%20EXT@19 f6b76eba-76ec-4392-87ed-a92b3dc2a7f6

--- DEV/babel.bat
+++ DEV/babel.bat
@@ -1,1 +1,1 @@
-node D:\corpse\projekty\project_watcher\ProjectWatcher.js -O project-watcher-options.json
+node D:\corpse\projekty\Project-Watcher-NodeJS\ProjectWatcher.js -O project-watcher-options.json
