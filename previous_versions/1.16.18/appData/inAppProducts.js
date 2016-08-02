4c2116165c317c6dd55a6f8e7ffdb56bcbc486ca
M!nified <gora.pub@gmail.com>
2016-08-02 14:44:26 +0200

    previous versions cleanup and zip

--- previous_versions/1.16.18/appData/inAppProducts.js
+++
@@ -1,9 +0,0 @@
-inAppProducts = [];
-
-inAppProducts["color_palette_background"]={sku:"color_palette_background",title:"Palette of 127 background colors",description:''};
-
-inAppProducts["color_background_454f56"]={sku:"color_background_454f56",title:"Background color: grey 1",description:'Color sample: <br><span style="margin-left:5px;display:inline-block;width:150px;height:100px;background-color:#454f56; border-radius:3px;"></span>'};
-inAppProducts["color_background_ff7171"]={sku:"color_background_ff7171",title:"Background color: red 1",description:'Color sample: <br><span style="margin-left:5px;display:inline-block;width:150px;height:100px;background-color:#ff7171; border-radius:3px;"></span>'};
-inAppProducts["color_background_ff4fc1"]={sku:"color_background_ff4fc1",title:"Background color: pink 1",description:'Color sample: <br><span style="margin-left:5px;display:inline-block;width:150px;height:100px;background-color:#ff4fc1; border-radius:3px;"></span>'};
-
-inAppProducts["speech_recognition"]={sku:"speech_recognition",title:"Speech recognition.",description:'Supportted languages available under'};
--- previous_versions/1.8.1/options/general.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	var currentPage = window.location.href.match(/\w+\.html/i)[0];
-	$(".menu1>a[href='"+currentPage+"']").addClass("current");
-})
